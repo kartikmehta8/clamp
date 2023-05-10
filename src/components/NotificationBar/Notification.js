31999c772a5426c7dbb2bd1454eaf1c01913100c
Kartik Mehta <kartikmehta202@gmail.com>
2023-05-10 11:42:51 +0530

    mobile-responsiveness

--- src/components/NotificationBar/Notification.js
+++ src/components/NotificationBar/Notification.js
@@ -3,22 +3,14 @@ import './notification.css';
 
 export default function Notification() {
   return (
-    <div>
-      <div className='notification-top-bar'>
-        <p>
-          Clamp is deployed on the Polygon mainnet but still undergoing testing.
-          There is a risk of losing your funds and cryptocurrency. To go back to
-          Clamp's main page{' '}
-          <a
-            href='https://joinclamp.com'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            click here
-          </a>
-          .
-        </p>
-      </div>
+    <div className='notification-top-bar'>
+      Clamp is deployed on the Polygon mainnet but still undergoing testing.
+      There is a risk of losing your funds and cryptocurrency. To go back to
+      Clamp's main page{' '}
+      <a href='https://joinclamp.com' target='_blank' rel='noopener noreferrer'>
+        click here
+      </a>
+      .
     </div>
   );
 }
